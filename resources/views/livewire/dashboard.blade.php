3649ebbe7149f61dd72ee7fe322a10e2715be78e
Krauser-alv <kevinedi52@gmail.com>
2024-03-07 15:03:03 -0600

    CHANGED: modify charts

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -80,6 +80,7 @@
                   </div>
               </div>
           </div>
+          <!-- COMENTARIO PARA CONTENEDOR DE GRAFICAS -->
           <div class="row mt-4">
               <div class="col-lg-4 col-md-6 mt-4 mb-4">
                   <div class="card z-index-2 ">
@@ -143,6 +144,72 @@
                   </div>
               </div>
           </div>
+          <!-- COMENTARIO PARA CONTENEDOR DE GRAFICAS -->
+          <div class="row mt-4">
+              <div class="col-lg-4 col-md-6 mt-4 mb-4">
+                  <div class="card z-index-2 ">
+                      <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2 bg-transparent">
+                          <div class="bg-gradient-primary shadow-primary border-radius-lg py-3 pe-1">
+                              <div class="chart">
+                                  <canvas id="chart-bars-prueba" class="chart-canvas" height="170"></canvas>
+                              </div>
+                          </div>
+                      </div>
+                      <div class="card-body">
+                          <h6 class="mb-0 ">Website Views</h6>
+                          <p class="text-sm ">Last Campaign Performance</p>
+                          <hr class="dark horizontal">
+                          <div class="d-flex ">
+                              <i class="material-icons text-sm my-auto me-1">schedule</i>
+                              <p class="mb-0 text-sm"> campaign sent 2 days ago </p>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              <div class="col-lg-4 col-md-6 mt-4 mb-4">
+                  <div class="card z-index-2  ">
+                      <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2 bg-transparent">
+                          <div class="bg-gradient-success shadow-success border-radius-lg py-3 pe-1">
+                              <div class="chart">
+                                  <canvas id="chart-line" class="chart-canvas" height="170"></canvas>
+                              </div>
+                          </div>
+                      </div>
+                      <div class="card-body">
+                          <h6 class="mb-0 "> Daily Sales </h6>
+                          <p class="text-sm "> (<span class="font-weight-bolder">+15%</span>) increase in today
+                              sales. </p>
+                          <hr class="dark horizontal">
+                          <div class="d-flex ">
+                              <i class="material-icons text-sm my-auto me-1">schedule</i>
+                              <p class="mb-0 text-sm"> updated 4 min ago </p>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              <div class="col-lg-4 mt-4 mb-3">
+                  <div class="card z-index-2 ">
+                      <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2 bg-transparent">
+                          <div class="bg-gradient-dark shadow-dark border-radius-lg py-3 pe-1">
+                              <div class="chart">
+                                  <canvas id="chart-line-tasks" class="chart-canvas" height="170"></canvas>
+                              </div>
+                          </div>
+                      </div>
+                      <div class="card-body">
+                          <h6 class="mb-0 ">Completed Tasks</h6>
+                          <p class="text-sm ">Last Campaign Performance</p>
+                          <hr class="dark horizontal">
+                          <div class="d-flex ">
+                              <i class="material-icons text-sm my-auto me-1">schedule</i>
+                              <p class="mb-0 text-sm">just updated</p>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+
+
           <div class="row mb-4">
               <div class="col-lg-8 col-md-6 mb-md-0 mb-4">
                   <div class="card">
@@ -643,7 +710,87 @@
               },
           },
       });
+// Nuevo script
+var ctx = document.getElementById("chart-bars-prueba").getContext("2d");
 
+new Chart(ctx, {
+    type: "bar",
+    data: {
+        labels: ["M", "T", "W", "T", "F", "S", "S"],
+        datasets: [{
+            label: "Sales",
+            tension: 0.4,
+            borderWidth: 0,
+            borderRadius: 4,
+            borderSkipped: false,
+            backgroundColor: "rgba(255, 255, 255, .8)",
+            data: [50, 50, 50, 50, 50, 50, 40],
+            maxBarThickness: 6
+        }, ],
+    },
+    options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+            legend: {
+                display: false,
+            }
+        },
+        interaction: {
+            intersect: false,
+            mode: 'index',
+        },
+        scales: {
+            y: {
+                grid: {
+                    drawBorder: false,
+                    display: true,
+                    drawOnChartArea: true,
+                    drawTicks: false,
+                    borderDash: [5, 5],
+                    color: 'rgba(255, 255, 255, .2)'
+                },
+                ticks: {
+                    suggestedMin: 0,
+                    suggestedMax: 500,
+                    beginAtZero: true,
+                    padding: 10,
+                    font: {
+                        size: 14,
+                        weight: 300,
+                        family: "Roboto",
+                        style: 'normal',
+                        lineHeight: 2
+                    },
+                    color: "#fff"
+                },
+            },
+            x: {
+                grid: {
+                    drawBorder: false,
+                    display: true,
+                    drawOnChartArea: true,
+                    drawTicks: false,
+                    borderDash: [5, 5],
+                    color: 'rgba(255, 255, 255, .2)'
+                },
+                ticks: {
+                    display: true,
+                    color: '#f8f9fa',
+                    padding: 10,
+                    font: {
+                        size: 14,
+                        weight: 300,
+                        family: "Roboto",
+                        style: 'normal',
+                        lineHeight: 2
+                    },
+                }
+            },
+        },
+    },
+});
+// fin
 
       var ctx2 = document.getElementById("chart-line").getContext("2d");
 
